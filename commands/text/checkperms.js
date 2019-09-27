4114d2fa2713088e542da98a5d42eb09acc45159
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-09-27 16:04:53 +0200

    Non-embed versions of messages
    - if bot doen't have permission to send embed message it will send normal message instead

--- commands/text/checkperms.js
+++ commands/text/checkperms.js
@@ -7,11 +7,6 @@ module.exports = {
 	help: "`checkperms` - bot will reposnd with list of it's permissions",
 	run: msg => {
 		let canDoEmbed = msg.channel.permissionsFor(msg.guild.me).has('EMBED_LINKS')
-		if (!canDoEmbed) {
-			console.log('no embed permission'.red)
-			msg.channel.send("I don't have permission 'embed links' on this channel")
-			return
-		}
 
 		var perms = msg.guild.me.permissions
 			.toArray()
@@ -22,6 +17,11 @@ module.exports = {
 			.setTitle('My permissions on this guild are:')
 			.setColor(0xff0000)
 			.setDescription(perms)
-		msg.channel.send(embed)
+
+		if (canDoEmbed) {
+			msg.channel.send(embed)
+			return
+		}
+		msg.channel.send(`**My permissions on this guild are:**\n${perms}`)
 	},
 }
