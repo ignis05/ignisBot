bfb205df90795e5bed301dbf0ef5b874ae5ec10f
ignis05 <48099798+ignis05@users.noreply.github.com>
2019-10-21 13:01:32 +0200

    Added log list and help info

--- commands/text/log.js
+++ commands/text/log.js
@@ -1,9 +1,12 @@
 var { saveConfig, config, ignisID } = require('../../res/Helpers.js')
+const { RichEmbed } = require('discord.js')
 
 module.exports = {
 	name: 'log',
 	aliases: ['track'],
 	desc: `Selects channels for moderation logs`,
+	help:
+		'`log list` - shows current log settings\n\n`log <msg / voice / mod>` - enables / disables logs of selected type in current channel\n\nmsg - logs deleted and updated messages\nvoice - logs voice channels activity\nmod - logs guild members joining / leaving / being kicked/banned/unbanned',
 	run: async msg => {
 		if (!msg.member.hasPermission('ADMINISTRATOR') && msg.author.id != ignisID) {
 			msg.reply("You don't have permission to use this command")
@@ -11,12 +14,78 @@ module.exports = {
 		}
 
 		// create config
-		if (!config[msg.guild.id].log) config[msg.guild.id].log = { msg: [], voice: [], mod: [] }
-		await saveConfig()
+		if (!config[msg.guild.id].log) {
+			config[msg.guild.id].log = { msg: [], voice: [], mod: [] }
+			await saveConfig()
+		}
 
 		let arg = msg.content.split(' ')[1]
 		console.log(arg)
 		switch (arg) {
+			case 'list':
+			case 'settings':
+				let log = config[msg.guild.id].log
+				if (msg.channel.permissionsFor(msg.guild.me).has('EMBED_LINKS')) {
+					var embed = new RichEmbed()
+						.setTitle('**Moderation logs settings:**')
+						.setColor(0x00ffff)
+						.setDescription(`Which activities are logged to which channels`)
+						.addField(
+							'Message log:',
+							log.msg.length > 0
+								? log.msg.map(chID => {
+										let channel = msg.channel.guild.channels.get(chID)
+										return channel ? channel.toString() : 'deleted channel - will be cleared on next attempt to send log'
+								  })
+								: 'no channels'
+						)
+						.addField(
+							'Voice log:',
+							log.voice.length > 0
+								? log.voice.map(chID => {
+										let channel = voice.channel.guild.channels.get(chID)
+										return channel ? channel.toString() : 'deleted channel - will be cleared on next attempt to send log'
+								  })
+								: 'no channels'
+						)
+						.addField(
+							'Moderation log:',
+							log.mod.length > 0
+								? log.mod.map(chID => {
+										let channel = mod.channel.guild.channels.get(chID)
+										return channel ? channel.toString() : 'deleted channel - will be cleared on next attempt to send log'
+								  })
+								: 'no channels'
+						)
+						.setFooter(`Use \`!log [type]\` in specific channel to enable/disable selected type of logs in that channel`)
+					msg.channel.send(embed)
+				} else {
+					msg.channel.send(
+						`Turn on embed links permission for better messages\nMessage: ${
+							log.msg.length > 0
+								? log.msg.map(chID => {
+										let channel = msg.channel.guild.channels.get(chID)
+										return channel ? channel.toString() : 'deleted channel - will be cleared on next attempt to send log'
+								  })
+								: 'no channels'
+						}\nVoice: ${
+							log.voice.length > 0
+								? log.voice.map(chID => {
+										let channel = voice.channel.guild.channels.get(chID)
+										return channel ? channel.toString() : 'deleted channel - will be cleared on next attempt to send log'
+								  })
+								: 'no channels'
+						}\nModeration: ${
+							log.mod.length > 0
+								? log.mod.map(chID => {
+										let channel = mod.channel.guild.channels.get(chID)
+										return channel ? channel.toString() : 'deleted channel - will be cleared on next attempt to send log'
+								  })
+								: 'no channels'
+						}`
+					)
+				}
+				break
 			case 'msg':
 			case 'message':
 			case 'text':
