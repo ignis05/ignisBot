8262209081a2d4d08d73f0996832a7dc67e6ef17
ignis05 <48099798+ignis05@users.noreply.github.com>
2020-04-15 13:34:14 +0200

    Added dependencies for voice functionality

---
+++ commands/text/play.js
@@ -0,0 +1,10 @@
+const path = require('path')
+module.exports = {
+	name: 'voice',
+	// aliases: ['say', 'repeat'],
+	// desc: `repeats user's message`,
+	// help: "`echo <message>` - bot will send a message that is exact copy of user's message except for the `echo` tag",
+	run: async msg => {
+		
+	},
+}
