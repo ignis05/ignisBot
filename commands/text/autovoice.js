6c9a61d6767362fcac127fdc0ba20f588c4214de
ignis05 <48099798+ignis05@users.noreply.github.com>
2020-04-25 16:00:38 +0200

    Merged autovoicefirst.js into autovoice.js

--- commands/text/autovoice.js
+++ commands/text/autovoice.js
@@ -65,7 +65,7 @@ async function autovoiceActivity(guild) {
 module.exports = {
 	name: 'autovoice',
 	desc: `enables automatic voice channel managment`,
-	help: "`autovoice <category_id>` - enables automatic management of voice channels in given category\n\n-bot will automatically create and delete voice channels in that category to make sure that there is **exactly one empty voice channel at all times**\n\n-category ID number can be copied using discord's developer mode",
+	help: "`autovoice <category_id>` - enables automatic management of voice channels in given category\n\n-bot will automatically create and delete voice channels in that category to make sure that there is **exactly one empty voice channel at all times**\n\n-category ID number can be copied using discord's developer mode, if no id is given bot will set up autovoice based on the voicechannel that user is currently in.\nIf command is used without parameter and being in voice channel, autovoice is disabled.\nYou can use `autovoice first <number>` to change number of first autovoice channel",
 	run: msg => {
 		if (!msg.member.hasPermission('MANAGE_CHANNELS') && msg.author.id != ignisID) {
 			msg.reply("You don't have permission to use this command")
@@ -76,6 +76,24 @@ module.exports = {
 		}
 
 		var command = msg.content.split(' ').filter(arg => arg != '')
+
+		// autovoice first
+		if (command[1] === 'first') {
+			let nr = command[2]
+			if (!isNaN(nr)) {
+				config[msg.guild.id].autoVoiceFirstChannel = parseInt(nr)
+				saveConfig(msg.channel, `First autovoice channel set to ${nr}`)
+				// set names
+				var voiceChannels = msg.guild.channels.cache.filter(channel => channel.type == 'voice' && channel.parentID == config[msg.guild.id].autoVoice).array()
+				voiceChannels.forEach((channel, iterator) => {
+					channel.setName((iterator + parseInt(nr)).toString())
+				})
+			} else {
+				msg.reply('Given value is NaN')
+			}
+			return
+		}
+
 		var channel = command[1] ? msg.guild.channels.cache.get(command[1]) : (msg.member.voice && msg.member.voice.channel && msg.member.voice.channel.parent) || null
 		if (channel) {
 			if (channel.type == 'category') {
@@ -89,7 +107,7 @@ module.exports = {
 				msg.channel.send(`autovoice enabled for category: ${channel.name}`)
 
 				autovoiceActivity(msg.guild)
-				let vChannel = channel.children.find(ch => ch.type == 'voice' && ch.permissionsFor(msg.guild.me).has('CONNECT'))
+				let vChannel = channel.children.find(ch => ch.type == 'voice')
 				if (vChannel) {
 					vChannel.name = config[msg.guild.id].autoVoiceFirstChannel
 				}
