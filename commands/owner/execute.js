f90a2b8e381aa5477b274460bb41d2c6fb7bff53
ignis05 <48099798+ignis05@users.noreply.github.com>
2020-05-02 18:21:03 +0200

    More execute options
    - execute from attachments
    - access to lodash, config and helpers inside execute

--- commands/owner/execute.js
+++ commands/owner/execute.js
@@ -1,16 +1,31 @@
+let _ = require('lodash')
+let config = require('../../data/config.json')
+let Helpers = require('../../res/Helpers')
+const fetch = require('node-fetch')
+
 module.exports = {
 	name: 'execute',
-	run: msg => {
-		var content = msg.content.split(' ').slice(1).join(' ')
-		if (!content) return msg.channel.send('No command provided')
-		let temp = content.split('```')
-		if (temp.length < 3) return msg.channel.send('Wrap your code as multi line code with: ```')
-		var command = temp[1].replace('\n', ';')
-		if (command.startsWith('js')) command = command.slice(2)
+	run: async msg => {
+		var attachment = msg.attachments.find(a => a.name.endsWith('.js'))
+
+		var content = msg.cleanContent.split(' ').slice(1).join(' ')
+		if (!content && !attachment) return msg.channel.send('No command provided')
+		var command = ''
+		if (attachment) {
+			let response = await fetch(attachment.attachment)
+			let data = await response.text()
+			if (!data) return msg.channel.send('Failed to read attachment')
+			command = data.replace('\n', ';')
+		} else {
+			let temp = content.split('```')
+			if (temp.length < 3) return msg.channel.send('Wrap your code as multi line code with: ```')
+			command = temp[1].replace('\n', ';')
+			if (command.startsWith('js')) command = command.slice(2)
+		}
 		console.log('executing:\n', command)
-		const func = Function('msg', command)
+		const func = Function('msg', '_', 'config', 'Helpers', command)
 		try {
-			func(msg)
+			func(msg, _, config, Helpers)
 		} catch (err) {
 			msg.channel.send(`error:\n${err}`)
 		}
